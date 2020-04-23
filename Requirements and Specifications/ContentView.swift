98ce9c3f1bb266cf3c3991ed3942aba1f1f11a5d
Larry Burris <lburris@mac.com>
2020-04-23 18:23:52 -0400

    Initial Commit

---
+++ Requirements and Specifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Requirements and Specifications
+//
+//  Created by Larry Burris on 4/23/20.
+//  Copyright © 2020 Larry Burris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
